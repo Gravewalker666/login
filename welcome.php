0c352d3e7f7bf502b44da2b1d406a52d228065b2
gravewalker666 <heshan.andris.pro@gmail.com>
2018-12-21 11:39:20 +0530

    added php

---
+++ welcome.php
@@ -0,0 +1,51 @@
+<?php
+    $name = $_GET["name"];
+
+?>
+
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>Hello, world!</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" name="viewport"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
+    <!--     Fonts and icons     -->
+    <link rel="stylesheet" type="text/css"
+          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons"/>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
+    <!-- Material Kit CSS -->
+    <link href="assets/css/material-kit.css?v=2.1.0" rel="stylesheet"/>
+</head>
+
+<body>
+<div class="page-header header-filter" data-parallax="true" style="background-image: url('assets/img/bg3.jpg')">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 ml-auto mr-auto">
+                <div class="brand text-center">
+                    <h1>Your title here</h1>
+                    <h3 class="title text-center">Subtitle</h3>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="main main-raised">
+    <div class="container">
+        <div class="section text-center">
+            <h2 class="title">
+                <?php
+                echo "welcome ";
+                echo $name;
+                ?>
+            </h2>
+        </div>
+    </div>
+</div>
+</body>
+
+</html>
